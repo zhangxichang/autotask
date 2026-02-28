0f224d5e72861b618de9f7b60e010df6c7eba74d
张喜昌 <zhang.xichang@outlook.com>
2026-02-28 20:25:01 +0800

    实现命令执行

--- cmd/at.go
+++ cmd/at.go
@@ -1,17 +1,39 @@
 package cmd
 
 import (
+	"autotask/cmd/at"
+	"os"
+	"os/exec"
+
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
 
 func AutoTaskCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:   "at",
 		Short: "调用 Docker 执行自动化任务",
-		Args:  cobra.MaximumNArgs(1),
+		Args:  cobra.MaximumNArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) == 0 {
 				return cmd.Help()
+			}
+			task_file_path := args[0]
+			task_name := args[1]
+			data, err := os.ReadFile(task_file_path)
+			if err != nil {
+				return err
+			}
+			tasks := at.Tasks{}
+			if err := yaml.Unmarshal(data, &tasks); err != nil {
+				return err
+			}
+			task := tasks.Tasks[task_name]
+			c := exec.Command("sh", "-c", task.Run)
+			c.Stdout = os.Stdout
+			c.Stderr = os.Stderr
+			if err := c.Run(); err != nil {
+				return err
 			}
 			return nil
 		},
