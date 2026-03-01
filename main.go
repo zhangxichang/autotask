5d00d8fe00d0f2a1cc199ddbc86f8556d93ea50d
张喜昌 <zhang.xichang@outlook.com>
2026-03-01 17:10:29 +0800

    添加HTTP服务

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
 func main() {
+	address := "127.0.0.1:8080"
+	fmt.Printf("WebUI地址: http://%v\n", address)
+	if err := http.ListenAndServe(address, http.FileServer(http.Dir("dist"))); err != nil {
+		fmt.Printf("%v", err)
+		os.Exit(-1)
+	}
 }
